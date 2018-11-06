d0353cf74eb2d6701c02d87303122e6531b65452
Ruel Alfonso Misa <ruel.g.misa@gmail.com>
2018-11-06 17:00:19 -0500

    updated portfolio website

--- src/components/About/Bio/Bio.js
+++ src/components/About/Bio/Bio.js
@@ -12,10 +12,34 @@ const bio = () => {
             <img className={ styles.ProfilePic } src={ ProfilePic } alt="profile_pic" />
             <div className={ styles.Skills }>
                 <h3>Skills</h3>
-                <p>JavaScript + React + Redux + Ruby + Ruby on Rails + CSS + HTML + Responsive Design</p>
+                <p>Programming Languages</p>
+                <ul>
+                    <li>JavaScript</li>
+                    <li>Ruby</li>    
+                </ul> 
+                <p>Frameworks/Libraries</p>
+                <ul>
+                    <li>React</li>
+                    <li>Redux</li>
+                    <li>Rails</li>
+                    <li>Bootstrap</li>
+                </ul>
+                <p>Markup/Design</p>
+                <ul>
+                    <li>Sass</li>
+                    <li>CSS3</li>
+                    <li>HTML5</li>
+                </ul>
+                <p>Other Technical Skills</p>
+                <ul>
+                    <li>PostgreSQL</li>
+                    <li>Git</li>
+                    <li>Responsive Design</li>
+                    <li>BEM methodology</li>
+                </ul>
             </div>
             <div className={ styles.ResumeLink }>
-                <a href="https://drive.google.com/file/d/1rTSWSyA_gK10n7O_TVkVKiVaXIGBNVU1/view?usp=sharing" target="_blank">Link to my Resume</a>
+                <a href="https://drive.google.com/file/d/17f-bw-VvV5s9fYfZmsOpWe7mzMJ1nI48/view?usp=sharing" target="_blank">Link to my Resume</a>
             </div>
         </section>
     );
--- src/containers/Projects/Projects.js
+++ src/containers/Projects/Projects.js
@@ -6,11 +6,11 @@ import Gallery from '../../components/ProjectBox/Gallery/Gallery';
 
 // Videos & Images 
 import Rubicks from '../../videos/rubicks.mp4';
-import BakeShack from '../../videos/bakeshack.mp4';
-import PaperPlanes from '../../videos/paper-planes.mp4';
 import Conquest from '../../videos/conquest.mp4';
 import Pokedex from '../../videos/pokedex.mp4';
-import Avatar from '../../videos/avatar.mp4';
+import Salon from '../../videos/salon.mp4';
+import Barter from '../../videos/barter-post.mp4';
+import Building from '../../videos/building-scout.mp4';
 
 // Styling
 import styles from './Projects.module.css';
@@ -25,7 +25,7 @@ class Projects extends Component {
                     id: 1,
                     title: 'Rubicks Academy',
                     description: "A Full-Stack Web Dashboard designed to act as a way for administrators to oversee the academy's operations. Demo login and password: rubick@admin.com, 111111",
-                    technologies: 'Ruby on Rails + CSS + PostgreSQL + Heroku',
+                    technologies: 'Ruby on Rails + CSS3 + PostgreSQL + Heroku',
                     video: Rubicks,
                     links: [
                         {
@@ -37,37 +37,11 @@ class Projects extends Component {
                         }
                     ]
                 },
-                {   
-                    id: 2,
-                    title: 'Bake Shack',
-                    description: "A Full-stack application designed for a local neighborhood bakery that sold cakes, muffins, and cookies as their highlighted products. The user interface was heavily inspired by Blur's Coffee & TV music video.",
-                    technologies: 'Sinatra + Ruby + CSS + PostgreSQL',
-                    video: BakeShack,
-                    links: [
-                        { 
-                            name: 'Github',
-                            url: "https://github.com/ruemisa/bakeshack"
-                        }
-                    ]
-                },
-                {   
-                    id: 3,
-                    title: 'Paper Planes',
-                    description: 'A Single Page Front-end application designed for an environment-friendly paper company. It is designed using CSS Grid for the layout. CSS animations were also integrated to provide a more dynamic User Interface.',
-                    technologies: 'JavaScript + CSS + HTML',
-                    video: PaperPlanes,
-                    links: [
-                        {
-                            name: 'Github',
-                            url: "https://github.com/ruemisa/munderdifflin"
-                        }
-                    ]
-                },
                 {
-                    id: 4,
+                    id: 2,
                     title: 'Conquest',
                     description: 'A Full-stack application designed for users who love to explore urban areas. Built a RESTful API through Ruby on Rails and  designed the User Interface through React. It is also designed using a mobile-first approach. The project is ongoing, meaning features are being added constantly. Demo login ( username: keltoh, password: 111111 ).',
-                    technologies: 'React + Rails + Google Maps API + CSS + PostgreSQL + Heroku',
+                    technologies: 'React + Rails + Google Maps API + CSS3 + PostgreSQL + Heroku',
                     video: Conquest, 
                     links: [
                         {
@@ -81,10 +55,10 @@ class Projects extends Component {
                     ]
                 },
                 {
-                    id: 5,
+                    id: 3,
                     title: "Strewbs' Pokedex",
                     description: 'A Single-Page Web application that provides useful information for three pokemon by making HTTP requests to the Pokemon RESTful API. Materialize CSS was integrated for the layout. The CSS was also written using SASS.',
-                    technologies: 'SASS + HTML + JavaScript + Materialize CSS + Pokemon API',
+                    technologies: 'SASS + HTML5 + JavaScript + Materialize CSS + Pokemon API',
                     video: Pokedex,
                     links: [
                         {
@@ -94,22 +68,47 @@ class Projects extends Component {
                     ]
                 },
                 {
-                    id: 6,
-                    title: "Avatar",
-                    description: 'A Full-stack web application designed as a social media platform for fans of the TV show: Avatar, the Last Airbender. It allows users to share images and thoughts through two types of communities: a global community and a community based on their allegiances. Demo logins: (user: nomad@gmail.com, password: 111111), (user: azula@email.com, password: 111111)',
-                    technologies: 'Sinatra + Ruby + CSS + PostgreSQL + Heroku',
-                    video: Avatar,
+                    id: 4,
+                    title: "Home Salon",
+                    description: 'A landing page layout of a company that provides hairstyling services at home. This project follows the BEM methodology and is responsive across multiple devices. Customized npm scripts were also created to build the final compiled css file.',
+                    technologies: 'SASS + HTML5 + CSS3',
+                    video: Salon,
                     links: [
                         {
                             name: 'Github',
-                            url: "https://github.com/ruemisa/rumblr"
-                        },
-                        {
-                            name: 'Heroku',
-                            url: "https://rumblr-ruel.herokuapp.com/"
+                            url: "https://github.com/ruemisa/home-salon"
                         }
                     ]
-
+                },
+                {
+                    id: 5,
+                    title: "Barter Trading Post",
+                    description: 'A user dashboard interface design of a barter trading auction house platform. This project uses Flexbox for the overall layout of the dashboard. It is responsive across multiple devices. Custom npm scripts were also created for building the compiled css file.',
+                    technologies: 'SASS + HTML5 + CSS3',
+                    video: Barter,
+                    links: [
+                        {
+                            name: 'Github',
+                            url: "https://github.com/ruemisa/barter-post"
+                        }
+                    ]
+                },
+                {
+                    id: 6,
+                    title: "Building Scout",
+                    description: 'A landing page layout of a company that provides tenant reviews about an apartment building. This project utilizes some basic css animation, and jQuery. It is responsive across multiple devices.',
+                    technologies: 'jQuery + CSS3 + HTML5',
+                    video: Building,
+                    links: [
+                        {
+                            name: 'Github',
+                            url: "https://github.com/ruemisa/building-scout"
+                        },
+                        {
+                            name: 'Github Pages',
+                            url: "https://ruemisa.github.io/building-scout/"
+                        }
+                    ]
                 }
 
             ],
